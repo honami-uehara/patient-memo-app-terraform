4ea3078cf312a31163d849bfa201a73ac15c9e17
honami-uehara <honaa_cos.627i@icloud.com>
2022-07-26 11:05:22 +0900

    ECS のrole 追加

--- aws_iam.tf
+++ aws_iam.tf
@@ -13,3 +13,9 @@ resource "aws_iam_user_policy_attachment" "deploy-attach" {
       user       = aws_iam_user.github_actions.name
       policy_arn = aws_iam_policy.deploy.arn
 }
+
+resource "aws_iam_role" "ecs_instance_role_for_patient_memo_app" {
+      name               = "ecs-instance-role-for-patient-memo-app"
+      path               = "/"
+      assume_role_policy = file("aws_iam_role_policies/ec2_assume_role_policy.json")
+  }
