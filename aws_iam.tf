95964c9643dfd7c775892d3c82776629ed7ec642
honami-uehara <honaa_cos.627i@icloud.com>
2022-07-26 17:26:26 +0900

    タスクロールの追加

--- aws_iam.tf
+++ aws_iam.tf
@@ -36,3 +36,9 @@ resource "aws_iam_role_policy_attachment" "ecs_instance_role_attach" {
   role = aws_iam_role.ecs_instance_role_for_patient_memo_app.name
   policy_arn = aws_iam_policy.ecs_instance_policy_for_patient_memo_app.arn
 }
+
+resource "aws_iam_role" "ecs_task_role_for_patient_memo_app" {
+  name               = "ecs-task-role-for-patient-memo-app"
+  path               = "/"
+  assume_role_policy = file("aws_iam_policies/ecs_task_assume_role_policy.json")
+}
