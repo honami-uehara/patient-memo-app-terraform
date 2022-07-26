b77df2f00a2aae45c24db05f3df0cc8a7140ba46
honami-uehara <honaa_cos.627i@icloud.com>
2022-07-26 22:05:08 +0900

    ecs serviceの追加

--- aws_ecs.tf
+++ aws_ecs.tf
@@ -8,3 +8,11 @@ resource "aws_ecs_task_definition" "patient_memo_app_task" {
   task_role_arn         = aws_iam_role.ecs_task_role_for_patient_memo_app.arn
   network_mode          = "bridge"
 }
+
+resource "aws_ecs_service" "rails_webapp_service" {
+  name            = "rails-todo-service"
+  cluster         = aws_ecs_cluster.ecs-cluster_patient_memo_app.id
+  task_definition = aws_ecs_task_definition.patient_memo_app_task.arn
+  desired_count   = 1
+  launch_type     = "EC2"
+}
