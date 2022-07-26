39206ca16b36a4c8cc32f5bd22fe40e8aa49078b
honami-uehara <honaa_cos.627i@icloud.com>
2022-07-26 14:25:39 +0900

    ロールにポリシーを紐付け

--- aws_iam.tf
+++ aws_iam.tf
@@ -34,3 +34,7 @@ resource "aws_iam_policy" "ecs_instance_policy_for_patient_memo_app" {
   policy      = file("aws_iam_policies/ecs_instance_policy.json")
 }
 
+resource "aws_iam_role_policy_attachment" "ecs_instance_role_attach" {
+    role = aws_iam_role.ecs_instance_role_for_patient-memo-app.name
+    policy_arn = aws_iam_policy.ecs_instance_policy_for_patient-memo-app.arn
+  }
