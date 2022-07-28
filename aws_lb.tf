409cb6bcb0011738ca920d6a279dc50b150c9b63
honami-uehara <honaa_cos.627i@icloud.com>
2022-07-28 20:07:36 +0900

    ロードバランサー のリスナーを追加

--- aws_lb.tf
+++ aws_lb.tf
@@ -17,3 +17,12 @@ resource "aws_lb_target_group" "http" {
     protocol = "HTTP"
     vpc_id = aws_vpc.vpc.id
 }
+
+resource "aws_lb_listener" "http" {
+      load_balancer_arn = aws_lb.lb.arn
+      port              = "80"
+      protocol          = "HTTP"
+      default_action {
+        target_group_arn = aws_lb_target_group.http.arn
+        type             = "forward"
+}
